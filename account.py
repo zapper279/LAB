57f0732f90e13b8b5a49f5db39b9af53059f0006
justinnicholas <jnicholas@unomaha.edu>
2022-11-17 12:03:53 -0600

    First File

--- account.py
+++ account.py
@@ -1,10 +1,22 @@
 class Account:
     def __init__(self, name):
+        self.__account_name = name
+        self.__account_balance = 0
 
     def deposit(self, amount):
+        if amount < 0 or amount == 0:
+            return False
+        if amount > 0:
+            return True
 
     def withdraw(self, amount):
+        if amount < 0 or amount == 0 or amount > self.__account_balance:
+            return False
+        if amount > 0:
+            return True
 
     def get_balance(self):
+        return self.__account_balance
 
     def get_name(self):
+        return self.__account_name
